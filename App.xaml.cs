5a74aa0363ab3b9f15c728bcc634b7e5af047fec
Thiago Sandre Trevisan <106849080+ThiagoST32@users.noreply.github.com>
2024-09-06 16:59:36 -0400

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace AppNumeroDaSorte
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //Current Page = Pagina Atual
+            MainPage = new MainPage();
+        }
+    }
+}
